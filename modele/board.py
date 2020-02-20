4fb8f1405986547cd2ab456260160a9069c97740
alexDussault <alex.dussault1@gmail.com>
2020-02-20 16:01:26 -0500

    Collision

--- modele/board.py
+++ modele/board.py
@@ -40,12 +40,12 @@ POINT = 0
 SCALING = 24
 DECALAGE = 85
 DECALAGEX = 12
-PACSPAWN = (14*SCALING,23*SCALING+DECALAGE)
-INKYSPAWN = (12*SCALING, 14*SCALING+DECALAGE)
-PINKYSPAWN = (14*SCALING, 14*SCALING+DECALAGE)
-CLYDESPAWN = (16*SCALING, 14*SCALING+DECALAGE)
-BLINKYSPAWN = (14*SCALING, 11*SCALING+DECALAGE)
-FANTOMES_SPAWN = {BLINKYSPAWN:'Blinky', PINKYSPAWN:'Pinky', INKYSPAWN:'Inky', CLYDESPAWN:'Clyde'}
+PACSPAWN = (14 * SCALING, 23 * SCALING + DECALAGE)
+INKYSPAWN = (12 * SCALING, 14 * SCALING + DECALAGE)
+PINKYSPAWN = (14 * SCALING, 14 * SCALING + DECALAGE)
+CLYDESPAWN = (16 * SCALING, 14 * SCALING + DECALAGE)
+BLINKYSPAWN = (14 * SCALING, 11 * SCALING + DECALAGE)
+FANTOMES_SPAWN = {BLINKYSPAWN: 'Blinky', PINKYSPAWN: 'Pinky', INKYSPAWN: 'Inky', CLYDESPAWN: 'Clyde'}
 
 
 def pastille():
@@ -53,7 +53,7 @@ def pastille():
     for ligne in range(len(GRILLE_DE_JEU)):
         for col in range(len(GRILLE_DE_JEU[ligne])):
             if GRILLE_DE_JEU[ligne][col] == POINT:
-                groupe.add(Pastille((col * SCALING + DECALAGEX, ligne * SCALING  + DECALAGE)))
+                groupe.add(Pastille((col * SCALING + DECALAGEX, ligne * SCALING + DECALAGE)))
     return groupe
 
 
@@ -65,16 +65,44 @@ def grosses_pastilles():
                 groupe.add(GrossePastille((col * SCALING + DECALAGEX, ligne * SCALING + DECALAGE)))
     return groupe
 
+
 def pac_init_pos():
     groupe = pygame.sprite.Group()
     groupe.add(PacMan(PACSPAWN))
     return groupe
+
 
 def fantomes_init_pos():
     groupe = pygame.sprite.Group()
     for spawn in FANTOMES_SPAWN.keys():
         groupe.add(Fantome(spawn, FANTOMES_SPAWN[spawn]))
     return groupe
+
+
+def est_un_mur(position):
+    return GRILLE_DE_JEU[position] == MUR
+
+
+def collision_mur(pacman: pygame.sprite.GroupSingle):
+    rect = pacman.sprite.rect
+    d = pacman.sprite.direction
+
+    if d == 0:  # Left
+        # Regarder si la position (rect.left,rect.y) **un coup ajusté a la grille** est un mur. on set la vitesse de pacman à 0.
+        pos_grille = ((rect.left) // SCALING - 1, (rect.y - DECALAGE) // SCALING)
+        return est_un_mur(pos_grille)
+
+    elif d == 1:  # Up
+        pos_grille = ((rect.x) // SCALING, (rect.top - DECALAGE) // SCALING - 1)
+        return est_un_mur(pos_grille)
+
+    elif d == 2: #Right
+        pos_grille = ((rect.right) // SCALING + 1, (rect.y - DECALAGE) // SCALING)
+        return est_un_mur(pos_grille)
+
+    else: # Down
+        pos_grille = ((rect.x) // SCALING, (rect.bottom - DECALAGE) // SCALING + 1)
+        return est_un_mur(pos_grille)
 
 
 class Pastille(pygame.sprite.Sprite):
@@ -87,17 +115,20 @@ class Pastille(pygame.sprite.Sprite):
 class GrossePastille(pygame.sprite.Sprite):
     def __init__(self, pos):
         pygame.sprite.Sprite.__init__(self)
-        self.images = [pygame.image.load(os.path.join('ressource', 'images', 'BigPellet.png')), pygame.image.load(os.path.join('ressource', 'images', 'Empty.png'))]
+        self.images = [pygame.image.load(os.path.join('ressource', 'images', 'BigPellet.png')),
+                       pygame.image.load(os.path.join('ressource', 'images', 'Empty.png'))]
         self.frame = 0
         self.image = self.images[self.frame]
         self.rect = self.image.get_rect(center=pos)
         self.isVisible = True
+
 
 class PacMan(pygame.sprite.Sprite):
     def __init__(self, pos):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(os.path.join('ressource', 'images', 'PacManLeft1.png'))
         self.rect = self.image.get_rect(center=pos)
+
 
 class Fantome(pygame.sprite.Sprite):
     def __init__(self, pos, nom):
