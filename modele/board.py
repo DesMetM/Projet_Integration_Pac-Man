e9e669b0277ca794cf6be3de9202a7c71027fae7
DRenaud <48636612+DesMetM@users.noreply.github.com>
2020-02-20 19:27:22 -0500

    pt quelque chose

--- modele/board.py
+++ modele/board.py
@@ -147,7 +147,7 @@ class PacMan(pygame.sprite.Sprite):
         self.GoDown = [0, self.CNSTE_VITESSE]
         self.vitesse = [0, 0]
 
-    def kill(self):
+    def kill_animation(self):
         self.pac.vitesse = [0, 0]
         self.pac.image = pygame.image.load(os.path.join(os.path.join('ressource', 'images', 'PacDead0.png')))
         pygame.time.wait(self.VITESSE_MORT)
